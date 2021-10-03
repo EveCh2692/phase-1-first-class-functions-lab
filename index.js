b4c520bc2d56a1b7fa167bb4476966d33c597c0f
Evelyn <evelyn.chavez92@yahoo.com>
2021-10-02 23:48:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const returnFirstTwoDrivers = function([Sally, Bob, Freddy, Claudia]){
-return Sally, Bob
+const returnFirstTwoDrivers = function(drivers){
+return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(driversTwo){
+    return driversTwo.slice(-2)
+}
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function(multiply){
+    return function(value){
+        return multiply * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function (drivers, driversToReturn){
+    return driversToReturn(drivers)
 }
